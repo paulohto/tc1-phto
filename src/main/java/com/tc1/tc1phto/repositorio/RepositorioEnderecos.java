c0e07f97e6ab57d51844c8dcb614f3ccd62af5be
Joney Pereira <joney.pereira.ext@conductor.com.br>
2023-08-06 09:14:23 -0300

    [jpa] feat: Add persistencias e repository

---
+++ src/main/java/com/tc1/tc1phto/repositorio/RepositorioEnderecos.java
@@ -0,0 +1,9 @@
+package com.tc1.tc1phto.repositorio;
+
+import com.tc1.tc1phto.dominio.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface RepositorioEnderecos extends JpaRepository<Endereco, Long> {
+}
