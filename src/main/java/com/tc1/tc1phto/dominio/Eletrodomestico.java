c4bbc4a0b7272a44610fbf2af08eaf6cc39716d7
Joney Pereira <joney.pereira.ext@conductor.com.br>
2023-08-06 08:18:06 -0300

    [jpa] feat: Add persistencias e repository

--- src/main/java/com/tc1/tc1phto/dominio/Eletrodomestico.java
+++ src/main/java/com/tc1/tc1phto/dominio/Eletrodomestico.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
-import java.math.BigDecimal;
 
 @Getter
 @Data
